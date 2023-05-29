c652a08d02fee63561874f94e1684ecac036c8ed
QuentinVdr <56938113+QuentinVdr@users.noreply.github.com>
2023-05-29 22:39:59 +0200

    ♿ Improve accessibility (#2)
    * :wheelchair: Improve accessibility
    
    * :arrow_up: Update dependency

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
         <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
             <NavBar />
             <AnimatePresence initial={false} mode="wait">
-              <Component key={router.asPath} {...pageProps} />
+                <Component key={router.asPath} {...pageProps} />
             </AnimatePresence>
             <Footer />
         </main>
