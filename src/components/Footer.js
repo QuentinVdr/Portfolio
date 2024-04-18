5cf0f3736fcf98bf501d8fe65a599267d48794c1
Quentin VERDIER <quent1.verdier@gmail.com>
2024-04-18 15:57:05 +0200

    🐛 Fix footer issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import React from "react";
 import Layout from "./Layout";
 
 const Footer = () => {
@@ -9,9 +8,12 @@ const Footer = () => {
     font-medium text-lg dark:text-light dark:border-light sm:text-base"
     >
       <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <span>2023 &copy; All Rights Reserved.</span>
         <div className="flex items-center lg:py-2">
-          Build With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
+          Build With&nbsp;
+          <span className="text-primary dark:text-primaryDark text-2xl px-1">
+            &#9825;
+          </span>
           by&nbsp;
           <Link
             href="https://github.com/QuentinVdr"
@@ -19,7 +21,7 @@ const Footer = () => {
             underline-offset-2"
             target={"_blank"}
           >
-            Quentin VERIER
+            Quentin VERDIER
           </Link>
         </div>
       </Layout>
